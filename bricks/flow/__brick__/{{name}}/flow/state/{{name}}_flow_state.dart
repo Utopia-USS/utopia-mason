52a974380ddd15689e334805b750a17383bf6582
Karol <kakol.karol00@gmail.com>
2022-07-23 16:32:18 +0200

    remove unnecesary

--- bricks/flow/__brick__/{{name}}/flow/state/{{name}}_flow_state.dart
+++ bricks/flow/__brick__/{{name}}/flow/state/{{name}}_flow_state.dart
@@ -1,12 +1,11 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
 
 class {{{name.pascalCase()}}}FlowState {
-
   const {{{name.pascalCase()}}}FlowState();
 }
 
 {{name.pascalCase()}}FlowState use{{name.pascalCase()}}FlowState({
   required Function() closeFlow,
 }) {
+
   return {{{name.pascalCase()}}}FlowState();
 }
